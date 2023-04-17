dc436c962d5e58f1cc3e31d8d45d1a2067144e87
Vladimir <vladimir.bur@overdrive.sg>
2023-04-18 00:46:52 +0300

    fix: created scratch class BinarySearchTree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,40 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor() {
+    this.treeRoot = null;
+  }
+  
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.treeRoot = addWithin(this.treeRoot, data);
+    
+    function addWithin(node, value) {
+      if (!node) {
+        return new Node(value);
+      }
+      
+      if (node.data === value) {
+        return node;
+      }
+      
+      if (value < node.data) {
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+      
+      return node;
+    }
   }
 
   has(/* data */) {
@@ -34,12 +53,32 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
+    if (!this.treeRoot) {
+      return;
+    }
+    
+    let node = this.treeRoot;
+    while (node.left) {
+      node = node.left;
+    }
+    
+    return node.data;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
+    if (!this.treeRoot) {
+      return;
+    }
+    
+    let node = this.treeRoot;
+    while (node.right) {
+      node = node.right;
+    }
+    
+    return node.data;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
